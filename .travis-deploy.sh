053743afefc94dcb625ef7728e1e4a72b262c996
Cody Coljee-Gray <cody.cg@gmail.com>
2017-04-09 12:17:33 -0700

    Another travis script fix.

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -6,9 +6,5 @@ git config user.email "travis@example.com"
 git add -f dist/
 git commit -m "Deploy to GitHub Pages"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-# tokens GH_TOKEN and GH_REF will be provided as Travis CI environment variables
-git subtree push --prefix dist "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+# Tokens GH_TOKEN and GH_REF will be provided as Travis CI environment variables
+git subtree push --prefix dist "https://${GH_TOKEN}@${GH_REF}" gh-pages
