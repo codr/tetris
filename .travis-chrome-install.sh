a18d0a00b43eaf9d41be13779ef74f2d2da450da
Cody Coljee-Gray <cody.cg@gmail.com>
2017-04-09 18:42:16 -0700

    set CHROME_BIN in the travis.yaml

--- .travis-chrome-install.sh
+++ .travis-chrome-install.sh
@@ -5,4 +5,3 @@ export FILE="chrome-linux.zip"
 curl "https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROMIUM_VERSION}/${FILE}" -s -w %{http_code} --create-dirs -o $FILE
 unzip $FILE -d $CHROMIUM_DIR
 rm $FILE
-export CHROME_BIN=$HOME/.chrome/chromium/chrome-linux/chrome
