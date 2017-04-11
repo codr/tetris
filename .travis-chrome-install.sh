d1987b84eb561e18dbe5da35ba0504310d27f043
Cody Coljee-Gray <cody.cg@gmail.com>
2017-04-10 21:37:52 -0700

    Trying chrome again.

--- .travis-chrome-install.sh
+++
@@ -1,7 +0,0 @@
-
-export CHROMIUM_VERSION=433059 # Chrome 53 linux stable, see https://www.chromium.org/developers/calendar
-export CHROMIUM_DIR=$HOME/.chrome/chromium
-export FILE="chrome-linux.zip"
-curl "https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROMIUM_VERSION}/${FILE}" -s -w %{http_code} --create-dirs -o $FILE
-unzip $FILE -d $CHROMIUM_DIR
-rm $FILE
