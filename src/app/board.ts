9a3fce4b91967640de605911695d81e1cfea2f87
Cody Coljee-Gray <cody.cg@gmail.com>
2021-12-28 20:51:35 -0800

    update board.ts

--- src/app/board.ts
+++ src/app/board.ts
@@ -6,17 +6,14 @@ export const NUMBER_COLS = 12;
 export class Board {
 
   static generateBoard() {
-    const board = Array(NUMBER_ROWS).fill(null);
-    board.forEach(Board.generateRow);
-    return board;
+    return Array.from({length: NUMBER_ROWS}, Board.generateRow)
   }
 
-  static generateRow(row, index, rows) {
-    rows[index] = Array(NUMBER_COLS).fill(null);
-    rows[index].forEach(Board.generateSquare);
+  static generateRow() {
+    return Array.from({length: NUMBER_COLS}, Board.generateSquare);
   }
 
-  static generateSquare(square, index, row) {
-    row[index] = new Square();
+  static generateSquare() {
+    return new Square();
   }
 }
