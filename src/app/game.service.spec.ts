ca78709be44770881a0ea0654156c89725733ab7
Cody Coljee-Gray <cody.cg@gmail.com>
2021-12-30 15:20:17 -0800

    Fix unit tests

--- src/app/game.service.spec.ts
+++ src/app/game.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { GameService } from './game.service';
 
 describe('GameService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [MatDialogModule],
       providers: [GameService]
     });
   });
--- src/app/stage/stage.component.spec.ts
+++ src/app/stage/stage.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { StageComponent } from './stage.component';
 import { GameService } from '../game.service';
@@ -10,6 +11,7 @@ describe('StageComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      imports: [MatDialogModule],
       declarations: [ StageComponent ],
       providers: [ GameService ],
       schemas: [ NO_ERRORS_SCHEMA ],
